5f99a5b3a32e77b7142a111875c99274e2bf9ebb
E-Mello <e.mello.dev@gmail.com>
2023-04-27 17:48:32 -0400

    Adjust all pages in PanelConfig for be a component

--- src/components/auth/Layout/index.tsx
+++ src/components/auth/Layout/index.tsx
@@ -1,23 +1,31 @@
 import Navbar from "~/components/auth/Navbar";
 import ProtectedRoute from "~/components/ProtectedRoute";
 import React from "react";
+import type { ReactNode } from "react";
 import { isActiveNavAtom } from "~/atoms/activeNavAtom";
 import { useAtom } from "jotai";
 
-export default function PanelConfig() {
+export default function Layout({
+  children,
+  ...props
+}: {
+  children: ReactNode;
+  [key: string]: any;
+}): JSX.Element {
   const [isActiveNav, setIsActiveNav] = useAtom(isActiveNavAtom);
   return (
     <ProtectedRoute>
       <div className="flex ">
         <Navbar />
         <section className={`${isActiveNav ? "ml-[15vw]" : "ml-[4vw]"}`}>
-          <main
+          <div
             className={`flex min-h-screen ${
               isActiveNav ? "w-[85vw]" : "w-[96vw]"
             } `}
+            {...props}
           >
-            {component && <component />}
-          </main>
+            {children}
+          </div>
         </section>
       </div>
     </ProtectedRoute>
---
+++ src/components/auth/content/menuinternalstandards/ActiviesData.tsx
@@ -0,0 +1,61 @@
+export default function ActiviesData() {
+  return (
+    <section className="flex w-full">
+      <section
+        className={`relative flex h-full w-full flex-col  items-center justify-between gap-10 bg-zinc-800 pt-[10vh] text-white`}
+      >
+        <h1 className="text-[2rem] font-bold">Painel de edição dos links</h1>
+        <div className="hover:bg-silver flex w-[81vw] flex-col justify-between gap-5 ">
+          <div className="flex flex-col gap-5">
+            <div>
+              <h1 className="text-lg font-bold">
+                Paginas relacionadas ao curso
+              </h1>
+            </div>
+            <div className="flex h-[20vh] w-[80vw] flex-row gap-5 whitespace-pre-line"></div>
+          </div>
+          <div className="flex flex-col gap-5">
+            <div>
+              <h1 className="text-lg font-bold">
+                Paginas relacionadas a Estrutura Curricular
+              </h1>
+            </div>
+            <div className="flex h-[20vh] w-[80vw] flex-row gap-5 whitespace-pre-line"></div>
+          </div>
+          <div className="flex flex-col gap-5">
+            <div>
+              <h1 className="text-lg font-bold">
+                Paginas relacionadas aos Eventos do Curso
+              </h1>
+            </div>
+            <div className="flex h-[20vh] w-[80vw] flex-row gap-5 whitespace-pre-line"></div>
+          </div>
+          <div className="flex flex-col gap-5">
+            <div>
+              <h1 className="text-lg font-bold">
+                Paginas relacionadas as Regulamentacoes Internas
+              </h1>
+            </div>
+            <div className="flex h-[20vh] w-[80vw] flex-row gap-5 whitespace-pre-line"></div>
+          </div>
+          <div className="flex flex-col gap-5">
+            <div>
+              <h1 className="text-lg font-bold">
+                Paginas relacionadas aos projetos do curso
+              </h1>
+            </div>
+            <div className="flex h-[20vh] w-[80vw] flex-row gap-5 whitespace-pre-line"></div>
+          </div>
+          <div className="flex flex-col gap-5">
+            <div>
+              <h1 className="text-lg font-bold">
+                Paginas relacionadas as Publicacoes do curso
+              </h1>
+            </div>
+            <div className="flex h-[20vh] w-[80vw] flex-row gap-5 whitespace-pre-line"></div>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+}
