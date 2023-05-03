d67616398f844938d14ff025f67a3dfd5582b2cb
E-Mello <e.mello.dev@gmail.com>
2023-05-02 23:01:45 -0400

    Superficially tweaked the look of the settings panel

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -1,3 +1,10 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "~/components/ui/accordion";
+
 import Layout from "~/components/admin/Layout";
 import type { NextPageWithLayout } from "~/types/layout";
 import type { ReactElement } from "react";
@@ -12,51 +19,86 @@ const DashboardCardInfo: NextPageWithLayout = () => {
         <div className="hover:bg-silver flex w-[81vw] flex-col justify-between gap-5 ">
           <div className="flex flex-col gap-5">
             <div>
-              <h1 className="text-lg font-bold">
-                Paginas relacionadas ao curso
-              </h1>
+              <Accordion type="single" collapsible>
+                <AccordionItem value="item-1">
+                  <AccordionTrigger>
+                    Páginas relacionadas ao Curso
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div className="flex flex-col gap-5">
+                      <div>
+                        <h1 className="text-lg font-bold">
+                          Paginas relacionadas ao Curso
+                        </h1>
+                      </div>
+                      <div className="flex h-[20vh] w-[80vw] flex-row gap-5 whitespace-pre-line"></div>
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-2">
+                  <AccordionTrigger>
+                    Paginas relacionadas a Estrutura Curricular
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div className="flex flex-col gap-5">
+                      <div>
+                        <h1 className="text-lg font-bold">
+                          Paginas relacionadas a Estrutura Curricular
+                        </h1>
+                      </div>
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-3">
+                  <AccordionTrigger>
+                    Paginas relacionadas aos Eventos do Curso
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div>
+                      <h1 className="text-lg font-bold">
+                        Paginas relacionadas aos Eventos do Curso
+                      </h1>
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-4">
+                  <AccordionTrigger>
+                    Paginas relacionadas as Regulamentacoes Internas
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div>
+                      <h1 className="text-lg font-bold">
+                        Paginas relacionadas as Regulamentacoes Internas
+                      </h1>
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-5">
+                  <AccordionTrigger>
+                    Paginas relacionadas aos projetos do curso
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div>
+                      <h1 className="text-lg font-bold">
+                        Paginas relacionadas aos projetos do curso
+                      </h1>
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+                <AccordionItem value="item-6">
+                  <AccordionTrigger>
+                    Paginas relacionadas as Publicacoes do curso
+                  </AccordionTrigger>
+                  <AccordionContent>
+                    <div>
+                      <h1 className="text-lg font-bold">
+                        Paginas relacionadas as Publicacoes do curso
+                      </h1>
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
             </div>
-            <div className="flex h-[20vh] w-[80vw] flex-row gap-5 whitespace-pre-line"></div>
-          </div>
-          <div className="flex flex-col gap-5">
-            <div>
-              <h1 className="text-lg font-bold">
-                Paginas relacionadas a Estrutura Curricular
-              </h1>
-            </div>
-            <div className="flex h-[20vh] w-[80vw] flex-row gap-5 whitespace-pre-line"></div>
-          </div>
-          <div className="flex flex-col gap-5">
-            <div>
-              <h1 className="text-lg font-bold">
-                Paginas relacionadas aos Eventos do Curso
-              </h1>
-            </div>
-            <div className="flex h-[20vh] w-[80vw] flex-row gap-5 whitespace-pre-line"></div>
-          </div>
-          <div className="flex flex-col gap-5">
-            <div>
-              <h1 className="text-lg font-bold">
-                Paginas relacionadas as Regulamentacoes Internas
-              </h1>
-            </div>
-            <div className="flex h-[20vh] w-[80vw] flex-row gap-5 whitespace-pre-line"></div>
-          </div>
-          <div className="flex flex-col gap-5">
-            <div>
-              <h1 className="text-lg font-bold">
-                Paginas relacionadas aos projetos do curso
-              </h1>
-            </div>
-            <div className="flex h-[20vh] w-[80vw] flex-row gap-5 whitespace-pre-line"></div>
-          </div>
-          <div className="flex flex-col gap-5">
-            <div>
-              <h1 className="text-lg font-bold">
-                Paginas relacionadas as Publicacoes do curso
-              </h1>
-            </div>
-            <div className="flex h-[20vh] w-[80vw] flex-row gap-5 whitespace-pre-line"></div>
           </div>
         </div>
       </section>
